11fdb2109ec797500c1129608016e6e204a05ce0
Somya-589 <93523150+Somya-589@users.noreply.github.com>
2022-07-19 20:55:19 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyADA-8tznwPOWlKMw-MUBZzOUWtiTpZaog",
+    authDomain: "letschatwebsite-c150e.firebaseapp.com",
+    databaseURL: "https://letschatwebsite-c150e-default-rtdb.firebaseio.com",
+    projectId: "letschatwebsite-c150e",
+    storageBucket: "letschatwebsite-c150e.appspot.com",
+    messagingSenderId: "846359318830",
+    appId: "1:846359318830:web:08b8266708ae62d82eeabf"
+  };
+firebase.initializeApp(firebaseConfig);
+  
